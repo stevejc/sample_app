07c5d754950ee9455213ac490eed0a9f68186ed5
Steve <stevemargc@gmail.com>
2013-01-03 21:45:40 -0500

    added a contact page, controller, changed dev and test db to postgres and guard/spork for testing

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,6 +1,7 @@
 require 'spec_helper'
 
 describe "Static pages" do
+  let(:base_title) { "Ruby on Rails Tutorial Sample App" }
   
   describe "home page" do
     it "should have the content 'Sample App'" do
@@ -11,7 +12,7 @@ describe "Static pages" do
     it "should have the right title" do
       visit '/static_pages/home'
       page.should have_selector('title',
-                        :text => "Ruby on Rails Tutorial Sample App | Home")
+                        :text => "#{base_title} | Home")
     end
   end
   
@@ -24,7 +25,7 @@ describe "Static pages" do
     it "should have the right title" do
       visit '/static_pages/help'
       page.should have_selector('title',
-                        :text => "Ruby on Rails Tutorial Sample App | Help")
+                        :text => "#{base_title} | Help")
     end
   end
   
@@ -37,7 +38,20 @@ describe "Static pages" do
     it "should have the right title" do
       visit '/static_pages/about'
       page.should have_selector('title',
-                        :text => "Ruby on Rails Tutorial Sample App | About Us")
+                        :text => "#{base_title} | About Us")
+    end
+  end
+  
+  describe "contact page" do
+    it "should have the content 'Contact'" do
+      visit '/static_pages/contact'
+      page.should have_selector('h1', :text => 'Contact')
+    end
+    
+    it "should have the right title" do
+      visit '/static_pages/contact'
+      page.should have_selector('title',
+                        :text => "#{base_title} | Contact")
     end
   end
   
